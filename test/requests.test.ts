b272b1a792a574304b2328ca2147c200c12a8738
Jhong098 <Jhong098@gmail.com>
2019-04-14 18:33:28 -0400

    add unit tests for request model

--- test/requests.test.ts
+++
@@ -1,9 +0,0 @@
-import app from "../lib/app";
-import * as request from "supertest";
-
-describe("GET /requests", () => {
-  it("200", async() => {
-    const res = await request(app).get("/requests");
-    expect(res.statusCode).toEqual(200);
-  });
-});
